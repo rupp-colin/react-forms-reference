4cbca3d5c04c87c3bb08bd220d44c0f252b9ed63
Colin Rupp <rupp.colin@gmail.com>
2019-12-10 11:31:58 -0800

    adds propTypes

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const TextInput = (props) => {
+
+  let formControl = 'form-control';
+
+  if (props.touched && !props.valid) {
+    formControl = 'form-control error';
+  }
+
   return (
-    <div classname="form-group">
-      <input type="text" className="form-control" {...props} />
+    <div className="form-group">
+      <input
+        type="text"
+        className={formControl}
+        name={props.name}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.onChange}
+      />
     </div>
   )
 }
 
 export default TextInput;
 
+// add proptypes
+TextInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func,
+  valid: PropTypes.bool.isRequired,
+  touched: PropTypes.bool.isRequired
+};
